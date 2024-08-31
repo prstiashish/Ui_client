756de5d5272d8a2c1db66aae9ebff93047431626
prstiashish <ashish.raymohapatra@prsti.tech>
2024-08-31 18:05:08 +0530

    Update AuthDetails.js

--- src/components/charts/AuthDetails.js
+++ src/components/charts/AuthDetails.js
@@ -18,9 +18,9 @@ export const baseURLs = () =>
   "https://sk5bgnkn3c.execute-api.ap-south-1.amazonaws.com/prod/salesdata/v1/";
 
 //dev
-// export const awsClientID = () => "6sv3ru6lpuljiuea0sdmshlvh8";
+export const awsClientID = () => "6sv3ru6lpuljiuea0sdmshlvh8";
 //Prod
-export const awsClientID = () => "3b41g664mjtaghfbtorjnf7639";
+// export const awsClientID = () => "3b41g664mjtaghfbtorjnf7639";
 
 export const baseAwsAuthenticateURL = () =>
   // "https://wex2emgh50.execute-api.ap-south-1.amazonaws.com/dev/authenticate";

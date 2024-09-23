3923b4dc641d95db68e78dd672bc3e8db179a0b1
prstiashish <ashish.raymohapatra@prsti.tech>
2024-09-23 12:20:10 +0530

    export updated

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,7 +3,7 @@ const awsconfig = {
   Auth: {
     region: "Mumbai",
     userPoolId: "ap-south-1_BcdMGSPra",
-    userPoolWebClientId: "6sv3ru6lpuljiuea0sdmshlvh8",
+    userPoolWebClientId: "5psbh9r0qv6hi9i5b9s7lk1bmr",
     mandatorySignIn: true,
   },
 };

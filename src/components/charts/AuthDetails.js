c7ece5ae0fb83a96e94b6b58856216376f0897ed
prstiashish <ashish.raymohapatra@prsti.tech>
2024-09-23 15:20:13 +0530

    Update AuthDetails.js trial-

--- src/components/charts/AuthDetails.js
+++ src/components/charts/AuthDetails.js
@@ -1,59 +1,11 @@
-import React, { useEffect, useState } from "react";
-
-export const GetAuthToken = () => {
-  if (typeof window !== "undefined") {
-    return sessionStorage.getItem("IdToken");
+window._config = {
+  cognito: {
+      userPoolId: 'ap-south-1_EBX2xGCWP', // e.g. us-east-2_uXboG5pAb
+      userPoolClientId: '5g2upknojgd0mngfjgva9otfo7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+      region: 'ap-south-1' // e.g. us-east-2
+  },
+  api: {
+      invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
   }
-  return null;
 };
 
-export const GetSchema = () => sessionStorage.getItem("Schema_name");
-
-export const GetTokenExpiredTime = () => sessionStorage.getItem("TokenExpiredTime");
-
-export const GetRefreshToken = () => sessionStorage.getItem("Refresh_Token");
-
-export const baseURLs = () =>
-  "https://wex2emgh50.execute-api.ap-south-1.amazonaws.com/dev/salesdata/v1/";
-  // "https://sk5bgnkn3c.execute-api.ap-south-1.amazonaws.com/prod/salesdata/v1/";
-
-//dev
-// export const awsClientID = () => "6sv3ru6lpuljiuea0sdmshlvh8";
-//Prod
-// export const awsClientID = () => "3b41g664mjtaghfbtorjnf7639";
-export const awsClientID = () => "5psbh9r0qv6hi9i5b9s7lk1bmr";
-
-export const baseAwsAuthenticateURL = () =>
-  "https://wex2emgh50.execute-api.ap-south-1.amazonaws.com/dev/authenticate";
-  // "https://sk5bgnkn3c.execute-api.ap-south-1.amazonaws.com/prod/authenticate/";
-
-// export const baseURLs = () => {
-//   debugger;
-
-//   if (process.env.NODE_ENV === "production") {
-//     return "https://wex2emgh50.execute-api.ap-south-1.amazonaws.com/dev/salesdata/v1/";
-//   } else {
-//     return "https://sk5bgnkn3c.execute-api.ap-south-1.amazonaws.com/prod/salesdata/v1/";
-//   }
-// };
-
-// export const awsClientID = () => {
-//   debugger;
-
-//   if (process.env.NODE_ENV === "production") {
-//     return "6sv3ru6lpuljiuea0sdmshlvh8";
-//   } else {
-//     return "3b41g664mjtaghfbtorjnf7639";
-//   }
-// };
-
-// "6sv3ru6lpuljiuea0sdmshlvh8";
-
-// export const baseAwsAuthenticateURL = () => {
-//   debugger;
-//   if (process.env.NODE_ENV === "production") {
-//     return "https://wex2emgh50.execute-api.ap-south-1.amazonaws.com/dev/authenticate";
-//   } else {
-//     return "https://sk5bgnkn3c.execute-api.ap-south-1.amazonaws.com/prod/authenticate/";
-//   }
-// };

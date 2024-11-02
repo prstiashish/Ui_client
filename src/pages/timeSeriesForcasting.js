ed3b432d3b138cfa38d796f37f6452a333cfc9e8
prstiashish <ashish.raymohapatra@prsti.tech>
2024-11-02 20:52:21 +0530

    updated

--- src/pages/timeSeriesForcasting.js
+++ src/pages/timeSeriesForcasting.js
@@ -8,7 +8,8 @@ import "@ag-grid-community/styles/ag-grid.css";
 import "@ag-grid-community/styles/ag-theme-quartz.css";
 import { ModuleRegistry } from "@ag-grid-community/core";
 import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
-import jsonData from "./allData";
+// import jsonData from "./allData";
+import jsonData from "../components/charts/timeSeriesData";
 import { DashboardLayout } from "src/components/dashboard-layout";
 
 
